719e3f05df38d0181f2c38eb77ecf1d628ffd0b5
APalaciosQ <93203495+APalaciosQ@users.noreply.github.com>
2024-01-27 11:11:52 -0400

    Fixing error in weapon physics

--- Assets/Scrips/PlayerAttack/ShooterController.cs
+++ Assets/Scrips/PlayerAttack/ShooterController.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class ShooterController : MonoBehaviour
 {
-    [SerializeField] private ShooterModel shooterModel; // Referencia a la arma actual
-    [SerializeField] private Transform bulletSpawnPoint;
-    [SerializeField] private Transform player; // Referencia al jugador
-    [SerializeField] private Vector3 gunOffset; // Desplazamiento de la pistola
-    [SerializeField] private float followSpeed = 1f; // Velocidad de seguimiento
-    private float nextFireTime;
+    [SerializeField] private WeaponModel    weaponModel; // Referencia a la arma actual
+    [SerializeField] private Transform      bulletSpawnPoint;
+    [SerializeField] private Transform      player; // Referencia al jugador
+    [SerializeField] private Vector3        gunOffset; // Desplazamiento de la pistola
+    [SerializeField] private float          followSpeed = 1f; // Velocidad de seguimiento
+
+    private float                           nextFireTime;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +19,16 @@ public class ShooterController : MonoBehaviour
         gunOffset = new Vector3(0.7f, -0.2f, -1);
     }
 
-    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        bulletSpawnPoint.localRotation = transform.localRotation;
+    }
+
     void Update() 
     {
         if (Input.GetButtonDown("Fire1") && Time.time > nextFireTime){
             shoot();
-            nextFireTime = Time.time + 1f / shooterModel.fireRate;
+            nextFireTime = Time.time + 1f / weaponModel.getFireRate();
         }
 
         // Hacer que el ShooterController siga al jugador con un retraso
@@ -33,19 +38,8 @@ public class ShooterController : MonoBehaviour
 
     void shoot () {
         // Instantiate the bullet at the position and rotation of the bulletSpawnPoint
-        GameObject bullet = Instantiate(shooterModel.bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        GameObject bullet = Instantiate(weaponModel.getBulletPrefab(), bulletSpawnPoint.position, bulletSpawnPoint.rotation);
 
-        // Calculate the direction vector from the ShooterController to the bulletSpawnPoint
-        Vector3 direction = (bulletSpawnPoint.position - transform.position).normalized;
-
-        // Make sure the bullet is fired in the correct direction
-        Rigidbody rb = bullet.GetComponent<Rigidbody>();
-        if (rb != null) {
-            BulletController bulletScript = bullet.GetComponent<BulletController>();
-            if (bulletScript != null) {
-            rb.velocity = direction;
-            }
-        }
     }
 
     void OnDrawGizmos () {
--- Assets/Scrips/PlayerAttack/ShooterModel.cs
+++ Assets/Scrips/PlayerAttack/ShooterModel.cs
@@ -2,9 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Shooter", menuName = "Shooter")]
-public class ShooterModel : ScriptableObject
+[CreateAssetMenu(fileName = "New Weapon", menuName = "Weapon")]
+public class WeaponModel : ScriptableObject
 {
-    public float fireRate;
-    public GameObject bulletPrefab;
+    [SerializeField] private float      fireRate;
+    [SerializeField] private float      weaponRecoil;
+    [SerializeField] private float      reloadTime;
+
+    [SerializeField] private int        magazine;
+    [SerializeField] private int        ammoRecerve;
+
+    [SerializeField] private GameObject bulletPrefab;
+
+    public float getFireRate()
+    {
+        return this.fireRate;
+    }
+
+    public float getWeaponRecoil()
+    {
+        return this.weaponRecoil;
+    }
+
+    public float getReloadTime()
+    {
+        return this.reloadTime;
+    }
+
+    public float getMagazine()
+    {
+        return this.magazine;
+    }
+
+    public float getAmmoRecerve()
+    {
+        return this.ammoRecerve;
+    }
+
+    public GameObject getBulletPrefab()
+    {
+        return this.bulletPrefab;
+    }
 }
